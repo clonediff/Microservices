ef5aa55544e2fde38c1581300b0e59e65fde318a
clonediff <32036592+clonediff@users.noreply.github.com>
2023-08-17 15:56:50 +0300

    [add] PlatformService with Dockerfile

---
+++ PlatformService/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseInMemoryDatabase("InMem"));
+
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.PrepPopulation();
+
+app.Run();
+
+// docker build -t clonediff/platformservice .              (Run from ../Microservices/PlatformService to build image)
+// docker run -p 8080:80 -d clonediff/platformservice       (Run from ../Microservices/PlatformService to launch image as new container)
+// docker ps                                                (Run to show already launched containers)
+// docker stop <container_id>                               (Run to stop launched container)
+// docker start <container_id>                              (Run to start existing container)
+// docker push clonediff/platformservice                    (Push image to DockerHub)
